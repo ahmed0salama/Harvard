6f872ca70c3dcb746d5377d67619d87c0ad8afb0
ahmed0salama <ahmedsalamassa12@gmail.com>
2026-08-10 19:43:41 +0300

    Django setup

---
+++ Django/hello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'hello/index.html')
+
+def greet(request, name):
+    return HttpResponse(f'Hello, {name.capitalize()}!')
