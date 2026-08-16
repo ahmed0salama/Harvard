00a418741d083c163a9e70942ff5becd253c457c
ahmed0salama <ahmedsalamassa12@gmail.com>
2026-08-17 02:15:11 +0300

    Create new app in Django called Flight

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('hello/', include('hello.urls')),
     path('newyear/', include('newyear.urls')),
     path('tasks/', include('tasks.urls')),
+    path('flight/', include('Flight.urls')),
 ]
