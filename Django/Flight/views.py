af4f5dcb3cabdb68225ed1be969c5a91236c95c4
ahmed0salama <ahmedsalamassa12@gmail.com>
2026-08-18 03:54:06 +0300

    Save progress

--- Django/Flight/views.py
+++ Django/Flight/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
-    return render(request, "flight/index.html")
+    return render(request, "flight/index.html", {"flights": Flight.objects.all(), 'lenght': len(Flight.objects.all())})
+
+def flight(request, flight_id):
+    f = Flight.objects.get(id=flight_id)
+    return render(request, "flight/flight.html", {'flight':f, 'passengers':Passenger.objects.filter(flights=f)})
