146ef90aadae26a84788efd95ea5260ed1fcabf5
ahmed0salama <ahmedsalamassa12@gmail.com>
2026-08-19 03:32:42 +0300

    Make app users work and understands how login logout works and how to authenticate users

--- Django/users/views.py
+++ Django/users/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    return render(request, "users/index.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    else:
+        return render(request, "users/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'users/login_view.html', {'message': 'Invalid credintial'})
+    return render(request, 'users/login_view.html')
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/login_view.html', {'message': 'Logged out'})
+
